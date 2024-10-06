75adf2cc3c24cc8d1f2b426094d0220ef60f024b
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-06 11:39:46 +0300

    Create AvatarEditor.jsx

---
+++ src/components/AvatarEditor.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AvatarEditor.css';
+
+const AvatarEditor = () => {
+  return (
+    <div className="avatar-editor">
+      <h2>Avatar Customization</h2>
+      <div className="avatar-preview">
+        <div className="avatar-cube"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AvatarEditor;

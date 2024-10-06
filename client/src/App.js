16e31d6320c200b0d6d02184ac5815655d9695d6
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-06 10:37:31 +0300

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AvatarEditor from './components/AvatarEditor';
+
+function App() {
+  return (
+    <div>
+      <h1>RepVid Avatar Customization</h1>
+      <AvatarEditor />
+    </div>
+  );
+}
+
+export default App;
